5e90649cbebfbc9f06b70697133af414f528058d
VGAparadise <5324416+vgaparadise@users.noreply.github.com>
2018-06-15 00:38:16 -0400

    Update example.php

--- example.php
+++ example.php
@@ -18,7 +18,7 @@
 	<meta name="msapplication-TileColor" content="#603cba">
 	<meta name="theme-color" content="#ffffff">
 
-	<title>Hehe I made the title xDD - ELTMaestro</title>
+	<title>Hehe I made the title xDDD - ELTMaestro</title>
 </head>
 
 
